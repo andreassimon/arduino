94728bb4fe58f47dbc65b00c5208dd3da929c04b
Andreas Simon <coding@andreas-simon.info>
2014-01-10 17:18:24 +0100

    parsed body

--- ethernet/server.ino
+++ ethernet/server.ino
@@ -1,6 +1,9 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
+const int VERTICAL_TAB = 13;
+
+
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
@@ -11,41 +14,12 @@ IPAddress ip(192,168,178,230);
 // (port 80 is default for HTTP):
 EthernetServer server(80);
 
-void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-
-
-  // start the Ethernet connection and the server:
-  Ethernet.begin(mac, ip);
-  server.begin();
-  Serial.print("server is at ");
-  Serial.println(Ethernet.localIP());
-}
-
-String queryParams(String *request) {
-  int firstSpace = (*request).indexOf(' ');
-  String uri = (*request).substring(firstSpace+1, (*request).indexOf(' ', firstSpace+1));
-  return uri.substring(uri.indexOf('?')+1);
-}
-
-String red(String _variableName, String *request) {
-  String variableName = _variableName + "=";
-  String queryParameters = queryParams(request);
-  int indexOfRed = queryParameters.indexOf(variableName);
-  int nextAmp = queryParameters.indexOf('&', indexOfRed+1);
-  return queryParameters.substring(indexOfRed, nextAmp);
-}
-
 class Request {
   String token;
 
   char nextChar(EthernetClient *client) {
     char c = (*client).read();
-    Serial.write(c);
+    // Serial.write(c);
     return c;
   }
 
@@ -78,9 +52,11 @@ class Request {
       }
 
       char c = nextChar(client);
+      if(int(c) == VERTICAL_TAB) { continue; }
       if(c == '\n') {
         if(currentLineIsBlank) {
-          readRemaining(client);
+          parseBody(client);
+          break;
         }
         currentLineIsBlank = true;
       } else {
@@ -91,21 +67,26 @@ class Request {
     return 0;
   }
 
-  int readRemaining(EthernetClient *client) {
+  int parseBody(EthernetClient *client) {
+    token = String();
+
     while ((*client).connected()) {
       if(!(*client).available()) {
         break;
       }
 
-      nextChar(client);
+      char c = nextChar(client);
+      token += c;
     }
 
+    body = token;
     return 0;
   }
 
   public:
     String method;
     String uri;
+    String body;
 
     int parse(EthernetClient *client) {
       token = String();
@@ -127,17 +108,42 @@ class Request {
     }
 };
 
-String request;
-int requestLength;
+
+Request request;
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  // start the Ethernet connection and the server:
+  request = Request();
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+String queryParams(String *request) {
+  int firstSpace = (*request).indexOf(' ');
+  String uri = (*request).substring(firstSpace+1, (*request).indexOf(' ', firstSpace+1));
+  return uri.substring(uri.indexOf('?')+1);
+}
+
+String red(String _variableName, String *request) {
+  String variableName = _variableName + "=";
+  String queryParameters = queryParams(request);
+  int indexOfRed = queryParameters.indexOf(variableName);
+  int nextAmp = queryParameters.indexOf('&', indexOfRed+1);
+  return queryParameters.substring(indexOfRed, nextAmp);
+}
+
 void loop() {
   // listen for incoming clients
   EthernetClient client = server.available();
   if (client) {
     Serial.println("new client");
-    request = String("");
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    Request request = Request();
     int status = request.parse(&client);
     delay(10);
     Serial.println();
@@ -146,6 +152,8 @@ void loop() {
     Serial.println(request.method);
     Serial.print("URI    >>> ");
     Serial.println(request.uri);
+    Serial.print("BODY   >>> ");
+    Serial.println(request.body);
 
     client.println("HTTP/1.1 200 OK");
     client.println("Content-Type: text/html");
@@ -155,8 +163,28 @@ void loop() {
     client.println("<html>");
     client.println("<body>");
     client.println("<form action=\"/\" method=\"POST\">");
-    client.println("<input type=\"text\" name=\"foo\" placeholder=\"foo\">");
-    client.println("<input type=\"text\" name=\"bar\" placeholder=\"bar\">");
+
+    client.println("<label>Red: </label>");
+    client.println("<select name=\"red\">");
+    client.println("  <option value=\"on\">ON</option> ");
+    client.println("  <option value=\"off\" selected>OFF</option>");
+    client.println("</select>");
+    client.println("<br>");
+
+    client.println("<label>Green: </label>");
+    client.println("<select name=\"green\">");
+    client.println("  <option value=\"on\">ON</option> ");
+    client.println("  <option value=\"off\" selected>OFF</option>");
+    client.println("</select>");
+    client.println("<br>");
+
+    client.println("<label>Blue: </label>");
+    client.println("<select name=\"blue\">");
+    client.println("  <option value=\"on\">ON</option> ");
+    client.println("  <option value=\"off\" selected>OFF</option>");
+    client.println("</select>");
+    client.println("<br>");
+
     client.println("<input type=\"submit\" value=\"Senden\">");
     client.println("</form>");
     client.println("</body>");
