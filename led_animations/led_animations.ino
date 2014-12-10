3239b66decd283137a8dc7d330c8af217c7188fd
Andreas Simon <coding@andreas-simon.info>
2014-12-10 20:52:06 +0100

    Moved Animations::Pulsating to library file

--- led_animations/led_animations.ino
+++ led_animations/led_animations.ino
@@ -21,51 +21,7 @@ void setup() {
 }
 
 #include "lib/Animations/Blink.cpp"
-
-namespace Animations {
-class Pulsating {
-  Adafruit_NeoPixel* strip;
-  unsigned int firstPixel, lastPixel;
-  int colorRed;
-  int lowerBoundary,
-      upperBoundary;
-
-  unsigned long lastChange, period, delta;
-
-  public:
-  Pulsating(Adafruit_NeoPixel* strip, unsigned int first, unsigned int last, int lowerBoundary, int upperBoundary) {
-    (*this).strip = strip;
-    firstPixel = first;
-    lastPixel = last;
-    period = 5;
-    (*this).lowerBoundary = lowerBoundary;
-    (*this).upperBoundary = upperBoundary;
-    colorRed = lowerBoundary;
-    delta = 1;
-    lastChange = millis();
-  }
-
-  void update() {
-    if(millis() - lastChange > period) {
-      lastChange = millis();
-      colorRed = colorRed + delta;
-      if(colorRed <= lowerBoundary) {
-        colorRed = lowerBoundary;
-        delta = -delta;
-      }
-      if(colorRed >= upperBoundary) {
-        colorRed = upperBoundary;
-        delta = -delta;
-      }
-      uint32_t color = (*strip).Color(colorRed, 0, 0);
-      for(uint16_t pixel = firstPixel; pixel <= lastPixel; pixel++) {
-        (*strip).setPixelColor(pixel, color);
-      }
-      (*strip).show();
-    }
-  }
-};
-}
+#include "lib/Animations/Pulsating.cpp"
 
 namespace Animations {
 const byte PAUSING = 0,
---
+++ led_animations/lib/Animations/Pulsating.cpp
@@ -0,0 +1,44 @@
+namespace Animations {
+  class Pulsating {
+    Adafruit_NeoPixel* strip;
+    unsigned int firstPixel, lastPixel;
+    int colorRed;
+    int lowerBoundary,
+        upperBoundary;
+
+    unsigned long lastChange, period, delta;
+
+    public:
+    Pulsating(Adafruit_NeoPixel* strip, unsigned int first, unsigned int last, int lowerBoundary, int upperBoundary) {
+      (*this).strip = strip;
+      firstPixel = first;
+      lastPixel = last;
+      period = 5;
+      (*this).lowerBoundary = lowerBoundary;
+      (*this).upperBoundary = upperBoundary;
+      colorRed = lowerBoundary;
+      delta = 1;
+      lastChange = millis();
+    }
+
+    void update() {
+      if(millis() - lastChange > period) {
+        lastChange = millis();
+        colorRed = colorRed + delta;
+        if(colorRed <= lowerBoundary) {
+          colorRed = lowerBoundary;
+          delta = -delta;
+        }
+        if(colorRed >= upperBoundary) {
+          colorRed = upperBoundary;
+          delta = -delta;
+        }
+        uint32_t color = (*strip).Color(colorRed, 0, 0);
+        for(uint16_t pixel = firstPixel; pixel <= lastPixel; pixel++) {
+          (*strip).setPixelColor(pixel, color);
+        }
+        (*strip).show();
+      }
+    }
+  };
+}
