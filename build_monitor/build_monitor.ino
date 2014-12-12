469ecffb26acbd52a498f4828c478e3ab11904c8
Andreas Simon <coding@andreas-simon.info>
2014-12-12 10:27:14 +0100

    Moved display functionality to the Job class

--- build_monitor/build_monitor.ino
+++ build_monitor/build_monitor.ino
@@ -5,6 +5,8 @@
 
 
 // #define DEBUG
+#define PIN 6
+#define NUM_PIXELS 40
 
 class Job {
   const char* host;
@@ -12,14 +14,23 @@ class Job {
   const char* uri;
   uint16_t firstPixel;
   uint16_t lastPixel;
+  Adafruit_NeoPixel* strip;
+  uint32_t green,
+           red,
+           white,
+           ledColor;
 
   public:
-  Job(const char* host, int port, const char* uri, uint16_t firstPixel, uint16_t lastPixel) {
+  Job(const char* host, int port, const char* uri, uint16_t firstPixel, uint16_t lastPixel, Adafruit_NeoPixel* strip) {
     (*this).host = host;
     (*this).port = port;
     (*this).uri = uri;
     (*this).firstPixel = firstPixel;
     (*this).lastPixel = lastPixel;
+    (*this).strip = strip;
+    (*this).green = (*strip).Color(  0,  50,   0);
+    (*this).red =   (*strip).Color(128,   0,   0);
+    (*this).white = (*strip).Color( 20,  20,  20);
   }
 
   const char* getHost() { return host; }
@@ -27,7 +38,48 @@ class Job {
   const char* getUri() { return uri; }
   uint16_t getFirstPixel() { return firstPixel; }
   uint16_t getLastPixel() { return lastPixel; }
+
+  uint32_t ledColorFromJobState(String jobState) {
+    if(String("blue") == jobState ||
+        String("blue_anime") == jobState)
+      return green;
+    if(String("red") == jobState ||
+        String("red_anime") == jobState)
+      return red;
+    return white;
+  }
+
+  void setPixels(uint16_t firstPixel, uint16_t lastPixel, uint32_t color) {
+    for(uint16_t pixel = firstPixel; pixel <= lastPixel; pixel++) {
+      (*strip).setPixelColor(pixel, color);
+    }
+    (*strip).show();
+  }
+
+  void setBuildColor(String buildColor) {
+    Serial.print(" => ");
+    ledColor = ledColorFromJobState(buildColor);
+    Serial.print(" setPixels( ");
+    Serial.print(firstPixel);
+    Serial.print(", ");
+    Serial.print(lastPixel);
+    Serial.print(", ");
+    Serial.print(ledColor);
+    Serial.println(" )");
+    setPixels(firstPixel, lastPixel, ledColor);
+    Serial.println();
+  }
 };
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
 
 // Enter the IP address for your controller below.
 // The IP address is dependent on your local network:
@@ -37,10 +89,10 @@ class Job {
   const IPAddress ip(10,10,11,13);
   const char* jenkins = "10.10.11.16";
   const Job jobs[] = {
-    Job( jenkins, 8080, "/job/blue/api/json",  0,  9 ),
-    Job( jenkins, 8080, "/job/blue-animated/api/json", 10, 19 ),
-    Job( jenkins, 8080, "/job/red/api/json", 20, 29 ),
-    Job( jenkins, 8080, "/job/red-animated/api/json", 30, 39 )
+    Job( jenkins, 8080, "/job/blue/api/json",  0,  9, &strip ),
+    Job( jenkins, 8080, "/job/blue-animated/api/json", 10, 19, &strip ),
+    Job( jenkins, 8080, "/job/red/api/json", 20, 29, &strip ),
+    Job( jenkins, 8080, "/job/red-animated/api/json", 30, 39, &strip )
   };
 
 /*
@@ -48,8 +100,8 @@ class Job {
   const IPAddress ip(192,168,1,128);
   const char *jenkins = "192.168.1.140";
   const Job jobs[] = {
-    { jenkins, 8080, "/job/earth-mage_ALL_Build/api/json",  0, 14 },
-    { jenkins, 8080, "/job/earth-mage_ALL_UnitTests/api/json", 15, 29 }
+    { jenkins, 8080, "/job/earth-mage_ALL_Build/api/json",  0, 14, &strip },
+    { jenkins, 8080, "/job/earth-mage_ALL_UnitTests/api/json", 15, 29, &strip }
   };
 */
 
@@ -60,23 +112,8 @@ Job currentJob = jobs[currentJobIndex];
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
-#define PIN 6
-#define NUM_PIXELS 40
-#define BLACK strip.Color(  0,   0,   0)
-#define WHITE strip.Color( 20,  20,  20)
-#define GREEN strip.Color(  0,  50,   0)
-#define BLUE  strip.Color(  0,   0,  30)
-#define RED   strip.Color(128,   0,   0)
 #define WAIT  400
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
 EthernetClient client;
 
 void GET(const char *host, const int port, const char *uri) {
@@ -138,31 +175,13 @@ void setup() {
   Serial.println(Ethernet.localIP());
 
   parser = JenkinsJobParser();
-  // colorWipe(RED  , 50);
-  // colorWipe(GREEN, 50);
-  // colorWipe(BLUE , 50);
-  // colorWipe(WHITE, 50);
-  // colorWipe(BLACK, 50);
+  // colorWipe(strip.Color(128,   0,   0), 50); // red
+  // colorWipe(strip.Color(  0,  50,   0), 50); // green
+  // colorWipe(strip.Color(  0,   0,  30), 50); // blue
+  // colorWipe(strip.Color( 20,  20,  20), 50); // white
+  // colorWipe(strip.Color(  0,   0,   0), 50); // black
 }
 
-char c;
-
-void setPixels(uint16_t firstPixel, uint16_t lastPixel, uint32_t color) {
-  for(uint16_t pixel = firstPixel; pixel <= lastPixel; pixel++) {
-    strip.setPixelColor(pixel, color);
-  }
-  strip.show();
-}
-
-uint32_t ledColorFromJobState(String jobState) {
-  if(String("blue") == jobState ||
-     String("blue_anime") == jobState)
-    return GREEN;
-  if(String("red") == jobState ||
-     String("red_anime") == jobState)
-    return RED;
-  return WHITE;
-}
 
 #ifdef DEBUG
 void printEthernetState(const int line) {
@@ -175,7 +194,6 @@ void printEthernetState(const int line) {
 }
 #endif
 
-uint32_t ledColor;
 void loop() {
 #ifdef DEBUG
   printEthernetState(__LINE__);
@@ -183,18 +201,8 @@ void loop() {
 
   if(!client.connected()) {
     client.stop();
-    Serial.print(" => ");
-    Serial.println(parser.getColor());
-    ledColor = ledColorFromJobState(parser.getColor());
-    Serial.print(" setPixels( ");
-    Serial.print(currentJob.getFirstPixel());
-    Serial.print(", ");
-    Serial.print(currentJob.getLastPixel());
-    Serial.print(", ");
-    Serial.print(ledColor);
-    Serial.println(" )");
-    setPixels(currentJob.getFirstPixel(), currentJob.getLastPixel(), ledColor);
-    Serial.println();
+
+    currentJob.setBuildColor(parser.getColor());
 
     currentJobIndex++;
     if(currentJobIndex >= numberOfJobs) {
@@ -206,8 +214,7 @@ void loop() {
     parser.reset();
   }
   if(client.available()) {
-    c = client.read();
-    parser.processResponseChar(c);
+    parser.processResponseChar(client.read());
   }
 
 #ifdef DEBUG
